d2a8fc4d94ff2512fcae25543badac94bedf1d80
Brian Denmark <testingarea479@gmail.com>
2022-07-28 20:39:43 -0700

    init commit

---
+++ JavaScript/arraydiff.js
@@ -0,0 +1,7 @@
+// Your goal in this kata is to implement a difference function, whch subtracts one list from another and returns the result. 
+
+// It should remove all values from list a, which are present in list b keeping their order. 
+// creat a function that takes two arrays as arguments
+    // iterate through the first array
+        // check if values of a are present in values of b
+        // if values are present in list a, and list b, remove values from list a
